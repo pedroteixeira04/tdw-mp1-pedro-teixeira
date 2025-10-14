7269375a6791d890612f12428d769c3d56609874
pedroteixeira04 <pfmt04@ua.pt>
2025-10-14 19:43:05 +0100

    fix: correct images configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: "export",
   images: {
     loader: "custom",
     formats: ["image/avif", "image/webp"],
-    images: { unoptimized: true },
+    unoptimized: true,
   },
 };
