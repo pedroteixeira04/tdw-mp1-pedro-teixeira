098af201716e1f072a998ff34f8601cfb5224dc0
pedroteixeira04 <pfmt04@ua.pt>
2025-10-18 12:41:52 +0100

    Update layout metadata and improve button hover styles; add period to section title

--- app/more-stories.tsx
+++ app/more-stories.tsx
@@ -44,7 +44,7 @@ export default function MoreStories({ morePosts }: { morePosts: any[] }) {
   return (
     <section>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight text-[#5EFF8C]">
-        Mais histórias
+        Mais histórias.
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32 text-white">
         {morePosts.map((post) => (
